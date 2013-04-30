e1029ddc820741d375365f8c53d9258060bdb43d
Apprentice <apprentice@devbootcamp.com>
2013-04-29 19:44:19 -0700

    game stops when won

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,14 +1,4 @@
 class Game < ActiveRecord::Base
   has_and_belongs_to_many :players
 
-  def check_winner(winner_id)
-    if winner_id == '1'
-      current_game.winner_id = session[:player_1_id]
-      current_game.save
-    else
-      current_game.winner_id = session[:player_2_id]
-      current_game.save
-    end
-  end
-
 end
